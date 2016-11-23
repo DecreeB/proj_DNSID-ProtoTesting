80abbef7d52cb65915ebb580b49f3de0959a73d6
DecreeB <decreeb@gmail.com>
2016-11-23 10:58:13 -0700

    Added Player State's
    Player States for default movement, crouching, and hidding are now
    added.

--- DNSID-Proto/Assets/Scripts/PlayerController2D.cs
+++ DNSID-Proto/Assets/Scripts/PlayerController2D.cs
@@ -2,15 +2,39 @@
 using System.Collections;
 using Prime31;
 
+/* 
+ * Player State Info
+ * 0 = Default
+ * 1 = Crouch
+ * 2 = Hide
+ * 3 = 
+ * */
+
 public class PlayerController2D : MonoBehaviour {
 
-	//Set Public Variables
+	//SET PUBLIC VARIABLES HERE
 	public GameObject gameCamera;
-	public float walkSpeed = 10;
-	public float gravity = -30;
-	public float jumpHeight = 2;
+	public int gravity = -50;
+	public float walkSpeed = 10f;
+	public float crouchSpeed = 5f;
+	public float jumpHeight = 5f;
+	public float crouchJumpHeight = 2f;
 
+	//SET PRIVATE VARIABLES HERE
+
+	//_controller is used for quick access to gameObject.GetComponent<CharacterController2D>
 	private CharacterController2D _controller;
+
+	//Sets the player's default state, grid listed above for conveniences
+	private int playerState = 0;
+
+	//How visible is the player?
+	private int playerVisible = 100;
+
+	//Stops players from doing things while hidden
+	private bool playerHidden = false;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -21,8 +45,51 @@ public class PlayerController2D : MonoBehaviour {
 
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	//FixedUpdate is called once per frame
+	void FixedUpdate () {
+
+		//SWITCH TO CROUCH STATE
+		if (Input.GetKeyDown (KeyCode.LeftControl) && playerHidden == false && _controller.isGrounded) {
+			if (playerState != 1) {
+				playerState = 1;
+				playerVisible = 50;
+			} else {
+				playerState = 0;
+				playerVisible = 100;
+			}
+		}
+
+		//SWITCH TO HIDDEN STATE
+		if (Input.GetKeyDown (KeyCode.E) && _controller.isGrounded){
+			if (playerState != 2) {
+				playerState = 2;
+				playerVisible = 0;
+				playerHidden = true;
+			} else {
+				playerState = 0;
+				playerVisible = 100;
+				playerHidden = false;
+			}
+		}
+
+		//SETS PLAYER STATES
+		switch (playerState)
+		{
+		case 0:
+			defaultMove (walkSpeed, jumpHeight);
+			break;
+		case 1:
+			defaultMove (crouchSpeed, crouchJumpHeight);
+			break;
+		case 2:
+
+			break;
+		}
+
+	}
+
+	//PLAYER MOVEMENT
+	void defaultMove (float walkSpeed, float jumpHeight) {
 
 		Vector3 velocity = _controller.velocity;
 
@@ -46,6 +113,7 @@ public class PlayerController2D : MonoBehaviour {
 		velocity.y += gravity * Time.deltaTime;
 
 		_controller.move (velocity * Time.deltaTime);
+
 	}
 
 }
